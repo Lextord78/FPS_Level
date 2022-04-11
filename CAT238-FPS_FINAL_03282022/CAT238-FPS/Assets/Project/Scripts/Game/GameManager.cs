2b24bfe41c37ae3ee2f0c3c6ef2c367f02d9cbe3
Lextord78 <alex_rodriguez@bloomfield.edu>
2022-04-11 17:39:23 -0400

    Update

--- CAT238-FPS_FINAL_03282022/CAT238-FPS/Assets/Project/Scripts/Game/GameManager.cs
+++ CAT238-FPS_FINAL_03282022/CAT238-FPS/Assets/Project/Scripts/Game/GameManager.cs
@@ -6,15 +6,19 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [Header("Game")]
+    public GameObject enemySpawner;
+
     public Player player;
     [Header("UI")]
     public Text ammoText;
     public Text healthText;
-
+    public Text enemyText;
+    public Text InfoText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InfoText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +26,29 @@ public class GameManager : MonoBehaviour
     {
         ammoText.text = "Ammo: " + player.Ammo;
         healthText.text = "Health: " + player.Health;
+        
+
+        int killedEnemies = 0;
+
+        foreach(Enemy enemy in enemySpawner.GetComponentsInChildren<Enemy>())
+        {
+            if(enemy.Killed == false)
+            {
+                killedEnemies++;
+            }
+        }
+        enemyText.text = "Enemies: " + killedEnemies;
+
+        if(killedEnemies == 0)
+        {
+            InfoText.gameObject.SetActive(true);
+        }
+
+        if(player.Killed == true)
+        {
+            InfoText.gameObject.SetActive(true);
+            InfoText.text = "you lose!";
+        }
+                
     }
 }
